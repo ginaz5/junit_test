d0a362e35f8a3ace193986712ffec1ff9d30fa90
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-04-22 23:44:10 +0800

    initial commit

---
+++ src/test/java/com/course/junit/helper/StringHelperTest.java
@@ -0,0 +1,13 @@
+package com.course.junit.helper;
+
+import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
+
+import static org.junit.Assert.*;
+
+public class StringHelperTest {
+
+    @Test
+    public void truncateAInFirst2Positions() {
+    }
+}
