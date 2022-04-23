7b6a11f2add69292fa23f8a24ceffae07026786d
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-04-23 11:09:11 +0800

    refactor code

--- src/test/java/com/course/junit/helper/StringHelperTest.java
+++ src/test/java/com/course/junit/helper/StringHelperTest.java
@@ -6,12 +6,16 @@ import org.junit.internal.runners.statements.Fail;
 import static org.junit.Assert.*;
 
 public class StringHelperTest {
+    StringHelper helper = new StringHelper();
 
     @Test
-    public void truncateAInFirst2Positions() {
-        StringHelper helper = new StringHelper();
+    public void truncateAInFirst2Positions_AinFirst2Positions() {
         // you can use refactor to make inline variable
         assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    public void truncateAInFirst2Positions_AinFirstPosition(){
         assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
     }
 }
