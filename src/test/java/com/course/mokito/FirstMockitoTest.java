0dc69c6ee46a58aaf5a54f7b5716de5d74161cdc
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-05-03 23:19:45 +0800

    - add Mockito dependency into pom.xml - add Mockito package

---
+++ src/test/java/com/course/mokito/FirstMockitoTest.java
@@ -0,0 +1,13 @@
+package com.course.mokito;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FirstMockitoTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
